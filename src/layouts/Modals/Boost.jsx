6b3450f25aff3f38ceb207a9635e530f955db622
minatnim <min.atnim@gmail.com>
2023-06-22 17:51:46 +0700

    editModal

--- src/layouts/Modals/Boost.jsx
+++ src/layouts/Modals/Boost.jsx
@@ -14,31 +14,40 @@ function Boost() {
         title={<img src="src/assets/iBoostS.svg" />}
         header={'BarTip Boost'}
       >
+        <br />
+
         <div className="w-40 mx-auto">
           <img src="src/assets/iBartender.svg" />
         </div>
-        <div className="flex">
-          <img src="src/assets/iTick.svg" className="w-10" />
-          <p className="py-4">เครื่องดื่มที่เหมาะกับคุณ</p>
-        </div>
-        <div className="flex">
-          <img src="src/assets/iTick.svg" className="w-10" />
-          <p className="py-4">หมวกที่บ่งบอกสไตล์ของคุณ</p>
-        </div>
-        <div className="flex items-start">
-          <img src="src/assets/iTick.svg" className="w-10" />
-          <div className="py-4">
-            ลดการดีเลย์ส่งข้อความตอนคุยกับเพื่อน
-            <div>เพื่อนในห้องได้ผลพลอยได้ไปด้วย</div>
+
+        <br />
+        <div className="flex flex-col px-9">
+          <div className="flex">
+            <img src="src/assets/iTick.svg" className="w-10" />
+            <div>เครื่องดื่มที่เหมาะกับคุณ</div>
+          </div>
+          <div className="flex">
+            <img src="src/assets/iTick.svg" className="w-10" />
+            <div>หมวกที่บ่งบอกสไตล์ของคุณ</div>
+          </div>
+          <div className="flex items-start">
+            <img src="src/assets/iTick.svg" className="w-10" />
+            <div>
+              ลดการดีเลย์ส่งข้อความตอนคุยกับเพื่อน
+              <div>เพื่อนในห้องได้ผลพลอยได้ไปด้วย</div>
+            </div>
+          </div>
+          <div className="flex items-start">
+            <img src="src/assets/iTick.svg" className="w-10" />
+            <div>
+              รีพอร์ตได้ 3 ครั้ง/วัน
+              <div>(ยูสเซอร์ปกติได้ 1 ครั้ง/วัน)</div>
+            </div>
           </div>
         </div>
-        <div className="flex items-start">
-          <img src="src/assets/iTick.svg" className="w-10" />
-          <div className="py-4">
-            รีพอร์ตได้ 3 ครั้ง/วัน
-            <div>(ยูสเซอร์ปกติได้ 1 ครั้ง/วัน)</div>
-          </div>
-        </div>
+        <br />
+        <br />
+        <br />
         <div onClick={handleClickBoost}>
           <Button text="ไปหน้าซื้อบูส" />
         </div>
--- src/layouts/Modals/BoostFriend.jsx
+++ src/layouts/Modals/BoostFriend.jsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import ModalsReuse from './ModalsReuse'
 
-const handleClick = () => {}
-
 function BoostFriend() {
-    return (
-        <div className="w-20" onClick={handleClick}>
-            <ModalsReuse
-                title={<img src="src/assets/iBoost.svg" />}
-                header={'ยินดีด้วย! มียูสเซอร์บูสอยู่ในห้องนี้'}
-            >
-                <img className="mx-auto" src="src/assets/iBoy.svg" />
+  const handleClick = () => {}
+  return (
+    <div className="w-20" onClick={handleClick}>
+      <ModalsReuse
+        title={<img src="src/assets/iBoost.svg" />}
+        header={'ยินดีด้วย! มียูสเซอร์บูสอยู่ในห้องนี้'}
+      >
+        <img className="mx-auto" src="src/assets/iBoy.svg" />
 
-                <p className="py-4">
-                    เค้ามีพลังลดการดีเลย์ในการส่งข้อความ ทำให้แชทกันได้เร็วขึ้น
-                    แถมเพื่อนๆ ในห้องได้ผลพลอยได้ไปด้วย
-                </p>
-            </ModalsReuse>
-        </div>
-    )
+        <p className="py-4">
+          เค้ามีพลังลดการดีเลย์ในการส่งข้อความ ทำให้แชทกันได้เร็วขึ้น แถมเพื่อนๆ
+          ในห้องได้ผลพลอยได้ไปด้วย
+        </p>
+      </ModalsReuse>
+    </div>
+  )
 }
 
 export default BoostFriend
