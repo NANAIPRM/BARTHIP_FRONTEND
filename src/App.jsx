75c0a86bf11c76e0a1c1aeb1ab81e3449fdaef8d
phoonsakkun <phoonsakkun@gmail.com>
2023-06-23 20:55:17 +0700

    register validate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import Router from './route/Router'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
-    return (
-        <div>
-            <Router />
-        </div>
-    )
+  return (
+    <div>
+      <Router />
+      <ToastContainer position="bottom-center" theme="dark" autoClose={4000} />
+    </div>
+  )
 }
 
 export default App
---
+++ src/api/auth-api.js
@@ -0,0 +1,9 @@
+// import axios from './axios'
+
+// const appApi = axios.create({
+//   baseURL: 'http://localhost:8080',
+// })
+
+// export const register = (input) => {
+//   return appApi.post('/auth/register', input)
+// }
