0048635155061067dc76a3ed3bd7fdb4d9f09fbf
Krittayot007 <krittayot.som@gmail.com>
2023-07-02 15:07:26 +0700

    fix .then to async await

--- src/features/ShopPage/components/ShopPageContainer.jsx
+++ src/features/ShopPage/components/ShopPageContainer.jsx
@@ -2,8 +2,14 @@ import { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import { AuthContext } from '../../../contexts/AuthContext'
 import { IiBuyitem, IiFace } from '../../../icons'
-import { addHatByUserId, addAvatarByUserId, addDrinkByUserId, getHatApi, getPostApi, getAvatarApi } from '../../../api/post-api'
-
+import {
+  addHatByUserId,
+  addAvatarByUserId,
+  addDrinkByUserId,
+  getHatApi,
+  getPostApi,
+  getAvatarApi,
+} from '../../../api/post-api'
 
 function ShopPageContainer() {
   const { user } = useContext(AuthContext)
@@ -11,53 +17,69 @@ function ShopPageContainer() {
   const [drink, setDrink] = useState([])
   const [avatar, setAvatar] = useState([])
 
-
   // ปุ่มกดซื้อProduct
   const hdlBuyHat = async (idx) => {
-    await addHatByUserId({ hatId: hat[idx].id });
-    const rs = await getHatApi();
-    setHat(rs.data);
-  };
+    try {
+      await addHatByUserId({ hatId: hat[idx].id })
+      const rs = await getHatApi()
+      setHat(rs.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   const hdlBuyDrink = async (idx) => {
-    await addDrinkByUserId({ drinkId: drink[idx].id });
-    const rs = await getPostApi();
-    setDrink(rs.data);
-  };
+    try {
+      await addDrinkByUserId({ drinkId: drink[idx].id })
+      const rs = await getPostApi()
+      setDrink(rs.data)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   const hdlBuyAvatar = async (idx) => {
-    await addAvatarByUserId({ avatarId: avatar[idx].id });
-    const rs = await getAvatarApi();
-    setAvatar(rs.data);
-  };
-
-  
-  useEffect(() => {
-    getHatApi().then(rs => {
-      setHat(rs.data)
-    })
-    getPostApi().then(rs => {
-      setDrink(rs.data)
-    })
-    getAvatarApi().then(rs => {
+    try {
+      await addAvatarByUserId({ avatarId: avatar[idx].id })
+      const rs = await getAvatarApi()
       setAvatar(rs.data)
-    })
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const hatResponse = await getHatApi()
+        setHat(hatResponse.data)
+
+        const drinkResponse = await getPostApi()
+        setDrink(drinkResponse.data)
+
+        const avatarResponse = await getAvatarApi()
+        setAvatar(avatarResponse.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchData()
   }, [])
-
-
 
   return (
     <>
       <div className="h-full flex flex-col justify-center items-center bg-gray-400">
-
-
-
-      
         <div className="flex flex-wrap w-[70%] bg--300 justify-center">
-          <h1 className="w-full m-4 font-semibold text-2xl flex justify-center">Hat</h1>
+          <h1 className="w-full m-4 font-semibold text-2xl flex justify-center">
+            Hat
+          </h1>
           {hat.map((el, idx) => (
             <>
-              <div className="flex flex-col justify-center items-center relative w-40 gap-4" key={idx}>
+              <div
+                className="flex flex-col justify-center items-center relative w-40 gap-4"
+                key={idx}
+              >
                 <img src="src/assets/iBoxbuy.svg" alt="Buybox" />
                 <div className="w-30 py-2 px-10 absolute top-5">
                   <img src={el.image} className="w-[60px]" />
@@ -65,19 +87,33 @@ function ShopPageContainer() {
                 <div>{el.name}</div>
                 <div>{el.description}</div>
                 <div>ราคา {el.price} บาท</div>
-                {el.UserHats[0]?.userId == user?.id ? <IiFace /> : <button className="w-20 cursor-pointer " onClick={() => { hdlBuyHat(idx) }}>
-                  <IiBuyitem />
-                </button>}
+                {el.UserHats[0]?.userId == user?.id ? (
+                  <IiFace />
+                ) : (
+                  <button
+                    className="w-20 cursor-pointer "
+                    onClick={() => {
+                      hdlBuyHat(idx)
+                    }}
+                  >
+                    <IiBuyitem />
+                  </button>
+                )}
               </div>
             </>
           ))}
         </div>
 
         <div className="flex flex-wrap w-[70%] bg--300 justify-center">
-          <h1 className="w-full m-4 font-semibold text-2xl flex justify-center">Drink</h1>
+          <h1 className="w-full m-4 font-semibold text-2xl flex justify-center">
+            Drink
+          </h1>
           {drink.map((el, idx) => (
             <>
-              <div className="flex flex-col justify-center items-center relative w-40 gap-4" key={idx}>
+              <div
+                className="flex flex-col justify-center items-center relative w-40 gap-4"
+                key={idx}
+              >
                 <img src="src/assets/iBoxbuy.svg" alt="Buybox" />
                 <div className="w-30 py-2 px-10 absolute top-5">
                   <img src={el.image} className="w-[60px]" />
@@ -85,19 +121,33 @@ function ShopPageContainer() {
                 <div>{el.name}</div>
                 <div>{el.description}</div>
                 <div>ราคา {el.price} บาท</div>
-                {el.UserDrinks[0]?.userId == user?.id ? <IiFace /> : <button className="w-20 cursor-pointer " onClick={() => { hdlBuyDrink(idx) }}>
-                  <IiBuyitem />
-                </button>}
+                {el.UserDrinks[0]?.userId == user?.id ? (
+                  <IiFace />
+                ) : (
+                  <button
+                    className="w-20 cursor-pointer "
+                    onClick={() => {
+                      hdlBuyDrink(idx)
+                    }}
+                  >
+                    <IiBuyitem />
+                  </button>
+                )}
               </div>
             </>
           ))}
         </div>
 
         <div className="flex flex-wrap w-[70%] bg--300 justify-center">
-          <h1 className="w-full m-4 font-semibold text-2xl flex justify-center">Avatar</h1>
+          <h1 className="w-full m-4 font-semibold text-2xl flex justify-center">
+            Avatar
+          </h1>
           {avatar.map((el, idx) => (
             <>
-              <div className="flex flex-col justify-center items-center relative w-40 gap-4" key={idx}>
+              <div
+                className="flex flex-col justify-center items-center relative w-40 gap-4"
+                key={idx}
+              >
                 <img src="src/assets/iBoxbuy.svg" alt="Buybox" />
                 <div className="w-30 py-2 px-10 absolute top-5">
                   <img src={el.image} className="w-[60px]" />
@@ -105,35 +155,25 @@ function ShopPageContainer() {
                 <div>{el.name}</div>
                 <div>{el.description}</div>
                 <div>ราคา {el.price} บาท</div>
-                {el.UserAvatars[0]?.userId == user?.id ? <IiFace /> : <button className="w-20 cursor-pointer " onClick={() => { hdlBuyAvatar(idx) }}>
-                  <IiBuyitem />
-                </button>}
+                {el.UserAvatars[0]?.userId == user?.id ? (
+                  <IiFace />
+                ) : (
+                  <button
+                    className="w-20 cursor-pointer "
+                    onClick={() => {
+                      hdlBuyAvatar(idx)
+                    }}
+                  >
+                    <IiBuyitem />
+                  </button>
+                )}
               </div>
             </>
           ))}
         </div>
-
-
-
-
-
       </div>
     </>
-
-
-
-
   )
 }
 
 export default ShopPageContainer
-
-
-
-
-
-
-
-
-
-
